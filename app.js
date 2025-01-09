4e8e77e1b094b286f61087a7d515800da8eaf58a
andrew hulme <andrew_hulme04@hotmail.com>
2025-01-09 18:16:05 +1100

    Created index.js model

--- app.js
+++ app.js
@@ -16,17 +16,5 @@ app.use(session({ secret: "mySecret", resave: false, saveUninitialized: false })
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get("/", (req, res) => res.render("index"));
-app.get("/register", (req, res) => res.render("register"));
-app.get("/login", (req, res) => res.render("login"));
-
-pool.query('SELECT NOW()', (err, res) => {
-  if (err) {
-    console.error('Error connecting to the database', err);
-  } else {
-    console.log('Database connected:', res.rows[0]);
-  }
-});
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
